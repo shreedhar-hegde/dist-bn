61b17de6d0dae3bcad43fbd809dbd5d23d3e68a5
Shreedhar Hegde <36230255+shreedhar-hegde@users.noreply.github.com>
2022-01-22 12:06:01 +0530

    Update dist-bn.js
    Error handling and JS doc

--- dist-bn.js
+++ dist-bn.js
@@ -1,3 +1,13 @@
+/**
+ *
+ * @param {string} string - String which contains words
+ * @param {string} firstWord - First word
+ * @param {number} firstWordPosition - Postion of occurence of first word. Should be greater than or equal to 1
+ * @param {string} secondWord - Second word
+ * @param {number} secondWordPosition  - Position of occurence of second word. Should be greater than or equal to 1
+ * @returns {number} - Distance between position of first and second word
+ */
+
 const distBn = (
   string,
   firstWord,
@@ -6,7 +16,31 @@ const distBn = (
   secondWordPosition
 ) => {
   const words = string.split(" ");
-  
+
+  if (!firstWord) {
+    throw Error("firstWord is not specified");
+  }
+
+  if (words.indexOf(firstWord) < 0) {
+    throw Error("firstWord does not exist in the given string");
+  }
+
+  if (!firstWordPosition) {
+    throw Error("firstWordPosition is not specified");
+  }
+
+  if (!secondWord) {
+    throw Error("secondWord is not specified");
+  }
+
+  if (words.indexOf(secondWord) < 0) {
+    throw Error("secondWord does not exist in the given string");
+  }
+
+  if (!secondWordPosition) {
+    throw Error("secondWordPosition is not specified");
+  }
+
   if (words.length < 2) {
     throw Error("String should contain at least 2 words");
   }
@@ -15,9 +49,10 @@ const distBn = (
     throw Error("Invalid first word. Length should be atleast 1");
   }
 
-  if (secondWord5length <= 0) {
+  if (secondWord.length <= 0) {
     throw Error("Invalid second word. Length should be atleast 1");
   }
+
   if (firstWordPosition <= 0 || secondWordPosition <= 0) {
     throw Error(
       "Invalid word position. Position should be greater than or equal to 1"
@@ -26,7 +61,6 @@ const distBn = (
 
   const uniqueWords = [...new Set(words)];
 
-  const indexes = [];
   let occ = {};
   uniqueWords.forEach((word) => {
     occ[word] = [];
@@ -43,5 +77,34 @@ const distBn = (
 
   return dist >= 0 ? dist : dist + 2;
 };
+
+const sentence1 =
+  "Now is the time to make real the promises of democracy. Now is the time to rise from the dark and desolate valley of segregation to the sunlit path of racial justice. Now is the time to lift our nation from the quicksands of racial injustice to the solid rock of brotherhood. Now is the time to make justice a reality for all of God's children.";
+
+let word1 = "justice";
+
+let word2 = "children.";
+
+console.log(
+  `Distance between '${word1}' and '${word2}' is`,
+  distBn(sentence1, word1, 1, word2, 1) + " words"
+);
+
+console.log(
+  `Distance between '${word2}' and '${word1}' is`,
+  distBn(sentence1, word2, 1, word1, 1) + " words"
+);
+
+const sentence2 =
+  "If you did know to whom I gave the ring, If you did know for whom I gave the ring, And would, conceive for what I gave the ring, And how unwillingly I left the ring, When naught would be accepted but the ring, You would abate the strength of your displeasure.";
+
+word1 = "whom";
+word2 = "whom";
+
+console.log(
+  `Distance between '${word1}' and '${word2}' is`,
+  distBn(sentence2, word1, 1, word2, 2),
+  "words"
+);
 
 module.exports = distBn;
